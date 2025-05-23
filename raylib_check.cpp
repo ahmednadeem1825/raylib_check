a054e01fa75b3fae89ca6eb854f050ece5836b62
ahmednadeem18 <bscs24129@itu.edu.pk>
2025-05-24 01:42:11 +0500

    hell

--- raylib_check.cpp
+++ raylib_check.cpp
@@ -9,7 +9,7 @@ int main() {
 
     const int screenWidth = 800;
     const int screenHeight = 600;
-    int ball_x = 100;
+    int ball_x = 100
     int ball_y = 100;
     int ball_speed_x = 5;
     int ball_speed_y = 5;
